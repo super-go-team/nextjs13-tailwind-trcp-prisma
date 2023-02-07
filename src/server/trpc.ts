91649b215a456edd421fb71fb722758f90f5ea29
Wade Montague - Super Go Team <wade@supergoteam.com>
2023-02-07 15:13:32 +0000

    add superjson transformer to maintain complex primitives

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,7 +1,10 @@
 import { Context } from "./context";
 import { initTRPC } from "@trpc/server";
+import SuperJSON from "superjson";
 
-const t = initTRPC.context<Context>().create({});
+const t = initTRPC.context<Context>().create({
+    transformer: SuperJSON
+});
 
 export const middleware = t.middleware;
 export const publicProcedure = t.procedure;
--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,6 +1,7 @@
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import type { AppRouter } from "@/server/routers/_app";
+import SuperJSON from "superjson";
 
 function getBaseUrl() {
   if (typeof window !== "undefined") {
@@ -18,6 +19,7 @@ function getBaseUrl() {
 export const api = createTRPCNext<AppRouter>({
   config({ ctx }) {
     return {
+      transformer: SuperJSON,
       links: [
         httpBatchLink({
           url: `${getBaseUrl()}/api`,
